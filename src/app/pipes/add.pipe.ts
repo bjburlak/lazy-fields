5128e00c35db27661c1601e95dc736d47769c9f9
Bruns <ryan.bruns@tylertech.com>
2018-09-19 12:37:20 -0400

    update the add pipe for 0 padding

--- src/app/pipes/add.pipe.ts
+++ src/app/pipes/add.pipe.ts
@@ -4,7 +4,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'add'
 })
 export class AddPipe implements PipeTransform {
-  transform(value: string): number {
-    return value ? parseInt(value, 10) + 1 : null;
+  transform(value: string): any {
+    const currentValue = value ? parseInt(value, 10) + 1 : null;
+    return currentValue && currentValue < 10 ? `0${currentValue}` : currentValue;
   }
 }
